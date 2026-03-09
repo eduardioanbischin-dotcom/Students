2f1eca18a8ea23fa2f3e2da82dfe854c71085edf
Eduard Bischin <eduardioan.bischin@ulbsibiu.ro>
2026-03-09 08:28:59 +0200

    Initial setup

--- src/main/java/ro/ulbs/proiectaresoftware/lab1/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/lab1/Application.java
@@ -2,18 +2,18 @@ package ro.ulbs.proiectaresoftware.lab1;
 
 public class Application {
     static void main() {
-         String alfabet_mare="";
-         String alfabet_mic=" ";
+        String alfabet_mare = "";
+        String alfabet_mic = " ";
 
-         for(int i=65;i<=90;i++){
-             String c= String.valueOf((char) i);
-alfabet_mare=alfabet_mare + c + " ";
+        for (int i = 65; i <= 90; i++) {
+            String c = String.valueOf((char) i);
+            alfabet_mare = alfabet_mare + c + " ";
 
 
-         }
-        for(int i=97;i<=122;i++){
-            String c= String.valueOf((char) i);
-            alfabet_mic=alfabet_mic + c + " ";
+        }
+        for (int i = 97; i <= 122; i++) {
+            String c = String.valueOf((char) i);
+            alfabet_mic = alfabet_mic + c + " ";
 
 
         }
